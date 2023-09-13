664fc26168a0d33d3a055ac3e8c2e70d61413c2f
surya kant <suryakant0013@yahoo.in>
2023-09-14 00:08:20 +0530

    origin and destination caputured and filled with details done with search button click

--- index.js
+++ index.js
@@ -6,27 +6,17 @@ import puppeteer from 'puppeteer';
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
+  await page.goto('https://www.irctc.co.in/');
 
   // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
-
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+//   await page.setViewport({width: 1080, height: 1024});
 
   // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-
-  await browser.close();
+  console.log('The irctc opened');
+  const fromElement  = await page.waitForSelector('#origin > span > input');
+await fromElement.type('H NIZAMUDDIN - NZM (NEW DELHI)');
+  const toStation = await page.waitForSelector('#destination > span > input');
+  await toStation.type('DEHRADUN - DDN ');
+  const searchButton = await page.waitForSelector("#divMain > div > app-main-page > div > div > div.level_2.slanted-div > div.col-xs-12.remove-padding.tbis-box.tbis-box-pad > div:nth-child(1) > app-jp-input > div > form > div:nth-child(5) > div.col-md-3.col-sm-12.col-xs-12.remove-pad > button");
+  await searchButton.click();
 })();
